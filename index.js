da148aecd5f74feec0f8560329b5042031e915b7
ROOHITH <rohith602@outlook.com>
2024-01-30 20:54:37 +0530

    latest cimmmit

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  $("#varibaleName").text("Rohith R");
 
-    $('#varibaleName').text("Rohith R");
-    
-    $('#varibaleAge').text("24");
-    
-    $('#varibaleIsStudent').text(false);
+  $("#varibaleAge").text("24");
+
+  $("#varibaleIsStudent").text(false);
+
+  function greet(name, age, isStudent) {
+    console.log("Hello, " + name + "!");
+    console.log(`Iam ${age} is old`);
+    console.log(`iam nor a ${isStudent}`);
+    $("#formated_string").text(
+      `Hello,  ${name} !, Iam ${age} is old iam nor a ${isStudent}`
+    );
+  }
+  greet(
+    $("#varibaleName").text(),
+    $("#varibaleAge").text(),
+    $("#varibaleIsStudent").text()
+  );
+  $("#varibalePerson").text(
+    JSON.stringify({
+      name: $("#varibaleName").text(),
+      age: $("#varibaleAge").text(),
+      isStudent: $("#varibaleIsStudent").text(),
+    })
+  );
 });
